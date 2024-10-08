45a19d2efceb051324f972186ceb714f35a03d05
Mahmudur <mahmudhera93@gmail.com>
2024-10-08 16:11:17 -0400

    use col major

--- compute_by_gpu.cu
+++ compute_by_gpu.cu
@@ -336,7 +336,7 @@ int main(int argc, char* argv[]) {
     // show the result, first 10x10
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            std::cout << result[i][j] << " ";
+            std::cout << results[i][j] << " ";
         }
         std::cout << std::endl;
     }
