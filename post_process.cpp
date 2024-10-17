d4f9c7b0b7f99ab1243a8f4b822bd84d3153f260
Mahmudur <mahmudhera93@gmail.com>
2024-10-17 18:15:32 -0400

    show info

--- post_process.cpp
+++ post_process.cpp
@@ -236,12 +236,16 @@ int main(int argc, char* argv[]) {
         int genome_id_this = genome_id_size_pairs[i].first;
         int size_this = genome_id_size_pairs[i].second;
         bool select_this = true;
+        // show my size
+        cout << "My size: " << size_this << endl;
         for (int j = 0; j < similars[genome_id_this].size(); j++) {
             int genome_id_other = similars[genome_id_this][j];
             if (genome_id_to_exclude[genome_id_other]) {
                 continue;
             }
             int size_other = sketches[genome_id_other].size();
+            cout << "Other: " << genome_id_other << endl;
+            cout << "Other size: " << size_other << endl;
             if (size_other > size_this) {
                 select_this = false;
                 cout << "Excluded " << genome_id_this << " since contained in " << genome_id_other << endl;
